c25bebe8ef2446540037c20fdb50da3e285fab71
Fishelstix <maxwellkf@mac.com>
2014-09-24 09:19:33 -0400

    Lab 1 in Homework 5

--- 05-codingBat/Driver.java
+++ 05-codingBat/Driver.java
@@ -12,6 +12,26 @@ public class Driver{
 	    return 21-n;
 	}
     }
+    public static boolean nearHundred(int n){
+	return Math.abs(n-100)<=10 || Math.abs(n-200)<=10;
+    }
+    public static boolean mixStart(String str) {
+	return str.substring(1,3).equals("ix");
+    }
+    public static int teaParty(int tea, int candy) {
+	if (tea >= 5 && candy >= 5) {
+	    if (tea >= 2 * candy || candy >= 2 * tea) {
+		return 2;
+	    }else{
+		return 1;
+	    }
+	}else{
+	    return 0;
+	}
+    }
+    public boolean lastDigit(int a, int b, int c) {
+	return (a%10==b%10)||(a%10==c%10)||(b%10==c%10);
+    }
     public static void main(String[] args){
 	System.out.println("");
 	System.out.println(nonStart("Hello", "There"));
