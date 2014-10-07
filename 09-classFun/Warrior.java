95acdc4ed884f6823f6d77a7942f93e50390b086
Fishelstix <Max@Maxwell-Fishelsons-27-iMac-i5.local>
2014-10-07 18:46:00 -0400

    Homework 9

---
+++ 09-classFun/Warrior.java
@@ -0,0 +1,8 @@
+public class Warrior extends Basechar {
+    // private int health= 50000;
+    private int healthboost = 5;
+    public int getHealth() {
+	return healthboost + super.getHealth();
+    }
+    
+}
