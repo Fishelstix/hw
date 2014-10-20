f008f6f4c289016ea28cd0d119419743d98030b1
Fishelstix <Max@Maxwell-Fishelsons-27-iMac-i5.local>
2014-10-20 19:56:04 -0400

    Stuyablo Basic v2

--- Stuyablo/Driver.java
+++ Stuyablo/Driver.java
@@ -18,24 +18,32 @@ public class Driver{
 	    bender = bendtype.nextLine().toLowerCase();
 	}
 	if(bender.equals("air")){
-	    Air player = new Air();
-	    System.out.println("\nWelcome to the arena "+player.getName()+" the "+bender+"bender!");
-	    player.playgame();
+	    Air p1 = new Air();
+	    opponent op1 = new opponent(3);
+	    System.out.println("\nWelcome to the arena "+p1.getName()+" the "+bender+"bender!");
+	    encounter boss = new encounter();
+	    boss.playgame(p1, op1);
 	}
 	if(bender.equals("water")){
-	    Water player = new Water();
-	    System.out.println("\nWelcome to the arena "+player.getName()+" the "+bender+"bender!");
-	    player.playgame();
+	    Water p1 = new Water();
+	    opponent op1 = new opponent(4);
+	    System.out.println("\nWelcome to the arena "+p1.getName()+" the "+bender+"bender!");
+	    encounter boss = new encounter();
+	    boss.playgame(p1, op1);
 	}
 	if(bender.equals("earth")){
-	    Earth player = new Earth();
-	    System.out.println("\nWelcome to the arena "+player.getName()+" the "+bender+"bender!");
-	    player.playgame();
+	    Earth p1 = new Earth();
+	    opponent op1 = new opponent(1);
+	    System.out.println("\nWelcome to the arena "+p1.getName()+" the "+bender+"bender!");
+	    encounter boss = new encounter();
+	    boss.playgame(p1, op1);
 	}
 	if(bender.equals("fire")){
-	    Fire player = new Fire();
-	    System.out.println("\nWelcome to the arena "+player.getName()+" the "+bender+"bender!");
-	    player.playgame();
+	    Fire p1 = new Fire();
+	    opponent op1 = new opponent(2);
+	    System.out.println("\nWelcome to the arena "+p1.getName()+" the "+bender+"bender!");
+	    encounter boss = new encounter();
+	    boss.playgame(p1, op1);
 	}
     }
 }
