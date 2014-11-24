79b0cf602dc30f9543e41628259e356d498d1bf7
fishelstix <maxwellkf@mac.com>
2014-11-24 09:28:27 -0500

    Word Search

--- 18-wordSearch/Driver.java
+++ 18-wordSearch/Driver.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
 public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch(7,10);
-	w.addWord("hello");
-	w.addWord("lol");
-	w.addWord("elo");
-	w.addWord("hell");
+	Scanner sc = null;
+	try {
+	    sc =new Scanner(new File("word"));
+	}catch (Exception e) {
+	    System.out.println("File not found");
+	    System.exit(0);
+	}
+	while (sc.hasNext()) {
+	    String s = sc.next();
+	    w.addWord(s);
+	}
 	System.out.println(w);
     }
 }
---
+++ 18-wordSearch/README.md
@@ -0,0 +1,5 @@
+Placement of words with random direction and location implemented.
+Placement of words that do not go off the board and intersect correctly implemented.
+Words read from a file.
+
+Yet to fill in rest of letters.
